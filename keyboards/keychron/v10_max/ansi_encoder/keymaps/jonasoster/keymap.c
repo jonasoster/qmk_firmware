aeff36f170bc785329d1cfac317e81db2165780c
Jonas Öster <jonas.oster@gmail.com>
2024-09-14 21:44:43 +0200

    jonasoster: clean up home row mods

--- keyboards/keychron/v10_max/ansi_encoder/keymaps/jonasoster/keymap.c
+++ keyboards/keychron/v10_max/ansi_encoder/keymaps/jonasoster/keymap.c
@@ -17,6 +17,15 @@
 #include QMK_KEYBOARD_H
 #include "keychron_common.h"
 
+#define HRM_A MT(MOD_LGUI,KC_A)
+#define HRM_S MT(MOD_LALT,KC_S)
+#define HRM_D MT(MOD_LCTL,KC_D)
+#define HRM_F MT(MOD_LSFT,KC_F)
+#define HRM_J MT(MOD_RSFT,KC_J)
+#define HRM_K MT(MOD_RCTL,KC_K)
+#define HRM_L MT(MOD_RALT,KC_L)
+#define HRM_SCLN MT(MOD_RGUI,KC_SCLN)
+
 enum layers {
     MAC_BASE,
     MAC_FN,
@@ -45,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_INS,             KC_DEL,
         MC_1,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MC_3,       KC_BSPC,  MT(MOD_LGUI,KC_A), MT(MOD_LALT,KC_S), MT(MOD_LCTL,KC_D), MT(MOD_LSFT,KC_F),   KC_G,      KC_H,     MT(MOD_RSFT,KC_J), MT(MOD_RCTL,KC_K), MT(MOD_RALT,KC_L), MT(MOD_RGUI,KC_SCLN),  KC_QUOT,            KC_ENT,             KC_HOME,
+        MC_3,       KC_BSPC,  HRM_A,    HRM_S,    HRM_D,    HRM_F,    KC_G,      KC_H,     HRM_J,    HRM_K,    HRM_L,    HRM_SCLN, KC_QUOT,            KC_ENT,             KC_HOME,
         MC_4,       KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP,
         MC_5,       KC_LCTL,  KC_LWIN,            KC_LALT,  KC_SPC,  MO(WIN_FN),                     KC_SPC,             KC_RALT,                      KC_LEFT,  KC_DOWN,  KC_RGHT),
 
